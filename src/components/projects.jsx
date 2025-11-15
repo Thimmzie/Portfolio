786d1623386385a2782e7d96f7e1526b7c0a4a9d
Ayodeji <thimmzieayodeji@gmail.com>
2025-11-15 09:47:08 +0100

    projects test further

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,7 +3,7 @@ import { Projects } from '../../constants/index.js';
 
 const projects = () => {
   return (
-    <div className="mb-[20rem] bg-[#e6e6e6] pt-[2rem] pb-[20rem]">
+    <div className="mb-[20rem] bg-[#f7f7f7] pt-[2rem] pb-[20rem]">
       <h1 className="text-[#000000] text-[1.5rem] pl-[2rem]">
         Recent Projects
       </h1>
@@ -12,16 +12,16 @@ const projects = () => {
           {Projects.map((detail) => {
             return (
               <div
-                className="bg-[#ffffff] w-[90%] rounded-[20px] border-1 border-[#c7c7c7] px-5 h-[55vh] flex flex-col justify-center"
+                className="bg-[#ffffff] w-[90%] rounded-[20px] border-1 border-[#c7c7c7] px-5 h-[47vh] flex flex-col justify-center"
                 key={detail.id}
               >
                 <p className="text-[1.2rem] font-[600]">{detail.title}</p>
-                <p className="text-[1.1rem] font-light pt-3 ">{detail.about}</p>
+                <p className="text-[0.9rem] font-light pt-3 ">{detail.about}</p>
                 <a
                   href={detail.link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-[#455ce9] mt-1 text-[1rem] mb-3"
+                  className="text-[#455ce9] mt-1 text-[0.8rem] mb-3"
                 >
                   View Project
                 </a>
