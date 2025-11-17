3cc7b98f9150fc3a0d753473f62e05add6fb43d7
Ayodeji <thimmzieayodeji@gmail.com>
2025-11-17 04:26:59 +0100

    projects

--- constants/index.js
+++ constants/index.js
@@ -14,7 +14,7 @@ const Projects = [
     id: 1,
     title: 'Dong',
     about:
-      'Dong is a Web3-powered reward and engagement platform built on blockchain technology. I worked on integrating...',
+      'Dong is a Web3-powered reward and engagement platform built on blockchain technology. I worked on integrating MetaMask wallet connectivity and interacting with the smart contract directly from the frontend, also built the frontend. The project focuses on enabling users to connect their wallets, view on-chain data, and perform secure transactions in a simple, user-friendly interface.',
     link: 'https://thimmzie.github.io/-Dong/',
     img: Dong,
   },
@@ -22,7 +22,7 @@ const Projects = [
     id: 2,
     title: 'Moneva',
     about:
-      'Moneva is a personal expense-tracking app designed to help users manage their daily spending with clarity and ease...',
+      'Moneva is a personal expense-tracking app designed to help users manage their daily spending with clarity and ease. It’s an ongoing project, with a minor frontend and major backend features still in development. This project focuses on a clean UI, smooth interactions, and real-time expense tracking.',
     link: 'https://moneva.pxxl.click/',
     img: Moneva,
   },
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Projects } from '../../constants/index.js';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const projects = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    let mm = gsap.matchMedia();
+    mm.add('(max-width: 768px)', () => {
+      gsap.utils.toArray('.project').forEach((card) => {
+        gsap.fromTo(
+          card,
+          { y: 80, opacity: 0 },
+          {
+            y: 0,
+            opacity: 1,
+            duration: 0.6,
+            ease: 'power3.out',
+            scrollTrigger: {
+              trigger: card,
+              start: 'top 85%',
+              toggleActions: 'play none none reverse',
+            },
+          }
+        );
+      });
+    });
+
+    mm.add('(min-width: 769px)', () => {
+      gsap.fromTo(
+        '.project',
+        { y: 80, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.7,
+          ease: 'power3.out',
+          stagger: 0.25, // second box animates slower
+          scrollTrigger: {
+            trigger: '.project',
+            start: 'top 90%',
+            toggleActions: 'play reverse play reverse',
+          },
+        }
+      );
+    });
+
+    return () => mm.revert();
+  }, []);
+
   return (
     <div className="mb-[20rem] bg-[#e7e7e7] pt-[5rem] pb-[7rem] lg:mt-[4rem]">
       <h1 className="text-[#000000] text-[1.5rem] pl-[2rem] lg:pl-[4.5rem] font-[600]">
@@ -12,18 +62,18 @@ const projects = () => {
           {Projects.map((detail) => {
             return (
               <div
-                className="bg-[#f8f8f8] w-[90%] rounded-[20px] border-1 border-[#c7c7c7] px-5 h-[53%] pt-[2rem] pb-[1rem] lg:h-[80vh]"
+                className="bg-[#f8f8f8] w-[90%] rounded-[20px] border-1 border-[#c7c7c7] px-5 h-[53%] pt-[2rem] pb-[1rem] lg:h-[80vh] project"
                 key={detail.id}
               >
                 <p className="text-[1.2rem] font-[600]">{detail.title}</p>
-                <p className="text-[1rem] font-[400] pt-3 tracking-wide lg:text-[0.9rem] ">
+                <p className="text-[0.9rem] font-[200] pt-3 tracking-wide lg:text-[0.9rem] line-clamp-2">
                   {detail.about}
                 </p>
                 <a
                   href={detail.link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-[#455ce9] mt-1 text-[0.8rem] mb-6"
+                  className="text-[#455ce9] mt-4 text-[0.8rem] mb-6 tracking-wider"
                 >
                   View Project
                 </a>
