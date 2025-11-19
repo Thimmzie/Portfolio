0f5be46d60b91d82bb792984e7eb4a74d189278b
Ayodeji <thimmzieayodeji@gmail.com>
2025-11-19 15:35:46 +0100

    footer animate

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { socials } from '../../constants/index.js';
+import gsap from 'gsap/all';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const footer = () => {
   const [time, setTime] = useState('');
@@ -7,23 +11,48 @@ const footer = () => {
   useEffect(() => {
     const update = () => {
       const now = new Date();
-      setTime(
-        now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-      );
+      const formatted = now.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true,
+      });
+      setTime(formatted);
     };
+
     update();
     const interval = setInterval(update, 1000);
-
     return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.footer-btn',
+        start: 'top 90%',
+        toggleActions: 'restart none none reverse',
+      },
+    });
+
+    tl.fromTo(
+      '.footer-btn',
+      { x: -100, opacity: 1 },
+      { x: 0, opacity: 1, duration: 1.2, ease: 'none' }
+    ).fromTo(
+      '.mail-btn',
+      { y: 110, opacity: 0 },
+      { y: 0, opacity: 1, duration: 0.7, ease: 'back.out(1.7)' },
+      '+=0.2'
+    );
+  }, []);
+
   return (
     <div className="h-[100vh] bg-[#1c1d20] pt-[3.5rem] px-[1.5rem]">
       <div>
-        <h2 className="text-[#ffffff] text-[2.3rem] tracking-wide pl-3">
+        <h2 className="text-[#ffffff] text-[2.3rem] tracking-wide pl-7">
           Let's work <span className="block -ml-[1.3rem]">together</span>
         </h2>
       </div>
-      <div className="flex justify-end pr-[1.3rem] -mt-2.5">
+      <div className="flex justify-end pr-[1.3rem] -mt-2">
         <button className="footer-btn">
           <a href="#">Get in touch</a>
         </button>
@@ -34,17 +63,17 @@ const footer = () => {
           <a href="#">thimmzieayodeji@gmail.com</a>
         </button>
       </div>
-      <div className="flex gap-8 mt-[6rem] justify-center">
+      <div className="flex gap-7 mt-[6rem]">
         {socials.map((med) => (
           <div key={med.id}>
             <a href="#">
               {' '}
-              <med.media size={38} className=" text-[white]" />
+              <med.media size={27} className=" text-[white]" />
             </a>
           </div>
         ))}
       </div>
-      <div className="flex justify-between mt-18">
+      <div className="flex justify-between mt-23 ">
         <p className="text-[#eff0f5] text-[0.9rem]">
           &copy; 2025 Ayodeji - Engineered
         </p>
