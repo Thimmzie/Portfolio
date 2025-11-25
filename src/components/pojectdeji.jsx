bec5413488973f92797fc13a1a05a6de25839e1b
Ayodeji <thimmzieayodeji@gmail.com>
2025-11-25 20:26:28 +0100

    added animation

--- src/components/pojectdeji.jsx
+++ src/components/pojectdeji.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FullProjects } from '../../constants/index.js';
 import { MdArrowOutward } from 'react-icons/md';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const pojectdeji = () => {
-  const colors = ['#e6f9ff', '#e9e9e9', '#e6fff9', '#e9e9e9'];
+  useEffect(() => {
+    gsap.utils.toArray('.project').forEach((item) => {
+      gsap.fromTo(
+        item,
+        { y: 100, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.45,
+          ease: 'power4.out',
+          scrollTrigger: {
+            trigger: item,
+            start: 'top 80%',
+            toggleActions: 'play reverse play reverse',
+          },
+        }
+      );
+    });
+  }, []);
+  const colors = ['#e6f9ff', '#f5f5f5', '#e6fff9', '#f5f5f5'];
   return (
-    <div className="mt-[3rem] mb-[50rem]">
+    <div className="mt-[3rem] mb-[5rem]">
       <h2 className="text-center text-[1.5rem] font-[600]">
         A collection of my work
       </h2>
       <div className="mt-[4rem] flex flex-col gap-12">
         {FullProjects.map((detail, i) => (
           <div
-            className=" px-6 py-8 rounded-[10px] w-[90%] mx-auto flex flex-col border-1 border-[#dddddd] bg-[#e9e9e9]"
+            className=" px-6 py-8 rounded-[10px] w-[90%] lg:w-[70%] mx-auto flex flex-col lg:flex-row lg:gap-4 border-1 border-[#dddddd] project"
             style={{ backgroundColor: colors[i % colors.length] }}
             key={detail.id}
           >
-            <div>
+            <div className="lg:w-[120%]">
               <h2 className="text-[1.3rem] font-[600]">{detail.title}</h2>
               <p className="py-4 text-[0.9rem] text-[#535353] font-[400] leading-7">
                 {detail.about}
               </p>
-              <p className="text-[1rem] text-[#6d6d6d]">{detail.stack}</p>
+              <p className="text-[1rem] text-[#6d6d6d] lg:text-[0.9rem] lg:text-[#252525]">
+                {detail.stack}
+              </p>
               <button className="flex gap-1 bg-[white] border-1  mt-4 mb-6 text-[0.8rem] tracking-wider rounded-[30px] py-2 px-4 border-[#aaaaaa] hover:bg-[#f3f3f3] cursor-pointer transition-all duration-300">
                 <a
                   href={detail.link}
@@ -34,8 +59,8 @@ const pojectdeji = () => {
                 <MdArrowOutward className="text-[#2b2b2b] " size={17} />
               </button>
             </div>
-            <div>
-              <img className="rounded-[8px]" src={detail.img} />
+            <div className="lg:mt-[2rem]">
+              <img className="rounded-[8px] " src={detail.img} />
             </div>
           </div>
         ))}
